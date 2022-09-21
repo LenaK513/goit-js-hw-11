153db58ae73d60c6dc200d10f483c2abacd6964c
Olena <kryvtsovaolena@gmail.com>
2022-09-21 23:29:05 +0300

    edit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/styles.css';
+import axios from 'axios'
+
+import Notiflix from 'notiflix';
+
+fetch('https://pixabay.com/api/?key=30064107-c73b2a0aceced325114b9b159&q=travel&image_type=photo&orientation=horizontal&safesearch=true')
